34a36f9cf5c23285556b28a968d543e9ccb0a7e5
Farmaan-Malik <farmaanmalik285@gmail.com>
2024-02-06 13:58:30 +0530

    First Commit

---
+++ app/src/main/java/com/example/myrecipeapp/MainViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.myrecipeapp
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+    private val _categoryState =  mutableStateOf(RecipeState())
+    val  categoryState : State<RecipeState> = _categoryState
+}
+
+data class RecipeState(
+    val loading: Boolean = true,
+    val list: List<Category> = emptyList(),
+    val error: String? = null
+)
